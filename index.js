ee646890a913fbfa46e008edcfb47c3e4690a1ce
TiparpronS <96738451+SukanyaT2022@users.noreply.github.com>
2024-02-29 11:03:40 -0500

    first

---
+++ index.js
@@ -0,0 +1,10 @@
+
+
+function radioChangeBG(id){
+    var storeBG = document.getElementById(id).value
+    document.body.style.backgroundColor = storeBG
+}
+function colorStyle(id){
+    var storeColorStyle = document.getElementById(id).value
+    document.getElementById('styleHeadingID').style.color = storeColorStyle
+}
