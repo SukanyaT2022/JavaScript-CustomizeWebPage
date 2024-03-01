b657d226e2f56bb9d9586a8b9f20e513b34fb467
TiparpronS <96738451+SukanyaT2022@users.noreply.github.com>
2024-03-01 10:57:43 -0500

    add

--- index.js
+++ index.js
@@ -8,3 +8,36 @@ function colorStyle(id){
     var storeColorStyle = document.getElementById(id).value
     document.getElementById('styleHeadingID').style.color = storeColorStyle
 }
+// alignment of the heading
+function changeAlignment(id){
+var storeAligment = document.getElementById(id).value
+document.getElementById('styleHeadingID').style.textAlign = storeAligment
+}
+
+// change heading font size
+function changeFontSize(){
+    var storeChangeFontSize = document.getElementById('changeFont').value
+    document.getElementById('styleHeadingID').style.fontSize = storeChangeFontSize + 'px'
+}
+
+//function for font style alignment underline
+
+function changeFontStyle(){
+    //.check property help to check 
+    // -if we select checkbox or deselect checkbox for italic
+if (document.getElementById('italic').checked==true){
+    document.getElementById('styleHeadingID').style.fontStyle = 'italic'
+}else{
+    document.getElementById('styleHeadingID').style.fontStyle = 'normal'
+}
+
+
+//.check property help to check 
+    // -if we select checkbox or deselect checkbox for underline
+    if (document.getElementById('underline').checked==true){
+        document.getElementById('styleHeadingID').style.textDecoration = 'underline'
+    }else{
+        document.getElementById('styleHeadingID').style.textDecoration = 'none'
+    }
+
+}
